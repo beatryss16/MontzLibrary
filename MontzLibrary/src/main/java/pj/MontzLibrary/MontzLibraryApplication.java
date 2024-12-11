9d0a4692570015854563748af61179295721db84
Anna Beatrys da Silva Souza <a.beatrys@academico.ifrn.edu.br>
2024-12-11 15:03:08 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ MontzLibrary/src/main/java/pj/MontzLibrary/MontzLibraryApplication.java
@@ -0,0 +1,13 @@
+package pj.MontzLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontzLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontzLibraryApplication.class, args);
+	}
+
+}
